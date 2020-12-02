7a645defb8b2ce4aef436ca0db7176988ce1b9a5
unknown <salazar_94@live.com.mx>
2020-12-02 13:31:58 -0700

    recupera fracciones

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Excel from "exceljs";
 
+import { writeFile } from "fs";
 
 
 class ExcelFraction {
@@ -8,20 +9,12 @@ class ExcelFraction {
         this.excel = new Excel.Workbook();
         this.worksheets = null;
         this.workBook = null;
-        this.formatFraction = RegExp('[0-9]{4}[.][0-9]{2}[.][0-9]{2}');
-    }
-
-    async getBook() {
-        return await this.excel.xlsx.readFile('./ECONOMIA_NICO-17-nov-20.xlsx');
+        this.path = './ECONOMIA_NICO-17-nov-20.xlsx';
+        this.formatFraction = RegExp(/\b([0-9]{4}[.][0-9]{2}[.][0-9]{2})\b/);
     }
 
     async getWorkBook() {
-        this.workBook = await this.getBook();
-    }
-
-    async getSheetNames() {
-        await this.getWorkBook();
-        return this.worksheets.worksheets.map(worksheet => worksheet.name);
+        return await this.excel.xlsx.readFile(this.path);
     }
 
     isFraction(fraction) {
@@ -29,32 +22,63 @@ class ExcelFraction {
     }
 
     async prinByTable() {
-        await this.getWorkBook();
-        const frac = []
-        this.workBook.eachSheet(async (w, id) => {
-            // console.log(id)
-            frac.push(await this.setFractions(id));
-        })
-        console.log(await frac)
+        const workBook = await this.getWorkBook();
+        const fractions = workBook.worksheets.map((sheet) => this.setFractions(sheet))
+        writeFile('helloworld.json', JSON.stringify(fractions), function (err) {
+            if (err) return console.log(err);
+            console.log('Hello World > helloworld.txt');
+        });
+        console.log(fractions)
+
     }
-    async setFractions(id) {
-        let m = await this.workBook.getWorksheet(id);
-        //  this.workBook.eachSheet()
-        const fracciones = [];
-        for (let index = 1; index < m.rowCount; index++) {
-            console.log(m.getRow(index).getCell(1).value)
-            let fraccion = m.getRow(index).getCell(1).value?.richText[0]?.text;
-            if (this.isFraction(fraccion)) {
-                let Nico = m.getRow(index).getCell(2).value.toString();
-                Nico = Nico > 9 ? Nico : `0${Nico}`;
-                fracciones.push({ fraccion: fraccion, nico: Nico })
+
+    setFractions(sheet) {
+        if (sheet.id > 104) return false;
+        try {
+            const fracciones = [];
+            const last = sheet.rowCount;
+            sheet.getColumn(1).style = { numFmt: 'yyyy.mm.dd' };
+            for (let index = 1; index < last; index++) {
+
+                let cellValue = sheet.getRow(index).getCell(1).value;
+                let fraccion = '';
+                if (this.isDate(cellValue)) {
+                    fraccion = this.fractionFromDate(cellValue);
+                } else {
+                    fraccion = sheet.getRow(index).getCell(1).value?.richText[0].text;
+                }
+                if (this.isFraction(fraccion)) {
+                    let Nico = sheet.getRow(index).getCell(2).value.toString();
+                    Nico = Nico > 9 ? Nico : `0${Nico}`;
+                    fracciones.push({ fraccion: fraccion, nico: Nico })
+                }
             }
+            return fracciones;
+        } catch (error) {
+            console.log(error)
         }
-        return fracciones;
+
+    }
+
+    fractionFromDate(dateLong) {
+        const date = new Date(dateLong);
+        return `${date.getFullYear()}.${this.appendZero(date.getUTCMonth() + 1)}.${this.appendZero(date.getUTCDate())}`
+    }
+
+    isDate(date) {
+        return Date.parse(date.toString()) > 0;
+    }
+
+    appendZero(number = 0) {
+        return number > 9 ? number : `0${number}`;
     }
 }
 
 const excel = new ExcelFraction();
-// let m = excel.getSheetNames()
-excel.prinByTable();
-// console.log(await m)
+
+try {
+    excel.prinByTable();
+} catch (error) {
+
+}
+
